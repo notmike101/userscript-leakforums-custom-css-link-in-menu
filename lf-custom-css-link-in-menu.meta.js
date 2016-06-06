66a6c2df2cb3efd94b2a2df0ff610417c4cd8d4e
Mike <IRDeNial@users.noreply.github.com>
2016-06-06 12:40:15 -0700

    Create lf-custom-css-link-in-menu.meta.js
    Meta data for userscript

---
+++ lf-custom-css-link-in-menu.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           LeakForums Custom CSS Link In Menu
+// @version        1.0.0
+// @description    Allow users to easily navigate to the custom CSS area.
+// @downloadURL    https://raw.githubusercontent.com/IRDeNial/LeakForums-Custom-CSS-Link-In-Menu/master/lf-custom-css-link-in-menu.user.js
+// @updateURL      https://raw.githubusercontent.com/IRDeNial/LeakForums-Custom-CSS-Link-In-Menu/master/lf-custom-css-link-in-menu.meta.js
+//
+// @author         IRDeNial / KVLT / https://leakforums.net/user-11865
+// @namespace      leakforums/11865/customcsslink
+//
+// @include        *://leakforums.net*
+// @include        *://www.leakforums.net*
+// @include        *://*.leakforums.net*
+//
+// @grant          metadata
+// ==/UserScript==
