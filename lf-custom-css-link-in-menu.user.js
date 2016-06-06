725ceadfedb1b5514c360538ac20c4f3dcbe48ff
Mike <IRDeNial@users.noreply.github.com>
2016-06-06 12:39:50 -0700

    Create lf-custom-css-link-in-menu.user.js
    Full userscript

---
+++ lf-custom-css-link-in-menu.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name           LeakForums Custom CSS Link In Menu
+// @version        1.0.0
+// @description    Allow users to easily navigate to the custom CSS area.
+// @downloadURL    https://raw.githubusercontent.com/IRDeNial/LeakForums-Custom-CSS-Link-In-Menu/master/lf-custom-css-link-in-menu.user.js
+// @updateURL      https://raw.githubusercontent.com/IRDeNial/LeakForums-Custom-CSS-Link-In-Menu/master/lf-custom-css-link-in-menu.meta.js
+//
+// @author         IRDeNial / KVLT / https://leakforums.net/user-11865
+// @namespace      leakforums/11865/customcsslink
+//
+// @include        *://leakforums.net*
+// @include        *://www.leakforums.net*
+// @include        *://*.leakforums.net*
+//
+// @grant          metadata
+// ==/UserScript==
+
+(function(){var b=document.querySelector('a[href="upgrade.php"]').parentNode.parentNode,a=document.createElement("li");a.innerHTML='<a href="customCSS.php">Custom CSS</a>';b.appendChild(a)})();
